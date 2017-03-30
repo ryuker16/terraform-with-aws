b7a412de084cc750efa0162e6c3af651892798f3
will-placester <wwilkins@placester.com>
2017-03-30 16:53:22 -0500

    added data template_file resource to main.tf to enable variables to be passed into policy_body on cloudstack formation

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 provider "aws" {}
 
+data "template_file" "firehose_delivery_role" {
+  template = "${file("${path.module}/templates/firehose_delivery_role.json")}"
+
+  vars {
+    LambdaArn       = "${aws_lambda_function.transformer.arn}"
+    BackupPrefix    = "${var.BackupPrefix}"
+    BucketARN       = "${aws_s3_bucket.property_canonical_bucket.arn}"
+    BackupBucketARN = "${aws_s3_bucket.property_extracted_bucket.arn}"
+  }
+}
+
 resource "aws_cloudformation_stack" "firehose_stack" {
   # https://www.terraform.io/docs/providers/aws/r/cloudformation_stack.html
   parameters {
@@ -25,5 +36,5 @@ resource "aws_cloudformation_stack" "firehose_stack" {
   }
 
   template_body = "${file("${path.module}/stack.yaml")}"
-  policy_body   = "${file("${path.module}/templates/firehose_delivery_role.json")}"
+  policy_body   = "${data.template_file.firehose_delivery_role.rendered}"
 }
